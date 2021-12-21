593bdcb6d1b913c64ddd3fc0a41bc73b17856288
mtoptas <mtoptas@invio.com>
2021-12-21 23:15:43 +0300

    theme implement add.

--- app/src/main/java/com/merttoptas/newsapp/MainActivity.kt
+++ app/src/main/java/com/merttoptas/newsapp/MainActivity.kt
@@ -14,6 +14,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            NewsApp{
+
+            }
         }
     }
 }
--- app/src/main/java/com/merttoptas/newsapp/ui/theme/Color.kt
+++ app/src/main/java/com/merttoptas/newsapp/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.merttoptas.newsapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val VeryDarkBlue= Color(0xFF222831)
+val DarkGreyBlue = Color(0xFF2c3458)
+val DarkGrayishBlue = Color(0xFF393E46)
+val StrongCyan = Color(0xFF00ADB5)
+val LightGray = Color(0xFFEEEEEE)
